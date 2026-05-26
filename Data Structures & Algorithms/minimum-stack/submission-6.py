a851a86cc1db50f9d2d152a1b90d89b34ce344cf
NG JIAN WEI <74456400+JayNg96@users.noreply.github.com>
2026-05-26 22:17:25 +0800

    Add: minimum-stack - submission-6

---
+++ Data Structures & Algorithms/minimum-stack/submission-6.py
@@ -0,0 +1,18 @@
+from collections import deque
+
+class MinStack:
+
+    def __init__(self):
+        self.stack = deque([])
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        
+    def pop(self) -> None:
+        self.stack.pop()
+         
+    def top(self) -> int:
+        return self.stack[-1] if self.stack else None
+
+    def getMin(self) -> int:
+        return min(self.stack)
