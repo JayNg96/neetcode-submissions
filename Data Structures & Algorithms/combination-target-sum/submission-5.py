bc4334888c44de198a367f04b327a888102e24e4
NG JIAN WEI <74456400+JayNg96@users.noreply.github.com>
2026-06-17 18:14:45 +0800

    Add: combination-target-sum - submission-5

---
+++ Data Structures & Algorithms/combination-target-sum/submission-5.py
@@ -0,0 +1,29 @@
+class Solution:
+    def combinationSum(self, nums: List[int], target: int) -> List[List[int]]:
+        if not nums:
+            return []
+
+        result = []
+        bag = []
+
+        def _helper(index = 0, total = 0):
+            if total == target:
+                if bag not in result:
+                    result.append(list(bag))
+                    return
+
+            if total > target:
+                return
+
+            if index == len(nums):
+                return
+
+            bag.append(nums[index])
+            _helper(index, total + nums[index])
+            bag.pop()
+            _helper(index + 1, total)
+
+            return result
+        
+        return _helper()
+
