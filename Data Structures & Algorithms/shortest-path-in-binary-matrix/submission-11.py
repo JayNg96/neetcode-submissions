86488d16f00df293aeedcc6dbea15a9011760dd1
NG JIAN WEI <74456400+JayNg96@users.noreply.github.com>
2026-06-25 20:43:31 +0800

    Add: shortest-path-in-binary-matrix - submission-11

---
+++ Data Structures & Algorithms/shortest-path-in-binary-matrix/submission-11.py
@@ -0,0 +1,47 @@
+class Solution:
+    def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
+        if grid[0][0] == 1 or grid[-1][-1] == 1:
+            return -1
+
+        ROWS = len(grid)
+        COLS = len(grid[0])
+        directions = [ [0,1], [0,-1], [1,0], [-1,0], [1, 1], [1,-1], [-1,1], [-1,-1] ]
+        queue = deque()
+        visited = set()
+        queue.append((0,0))
+        visited.add((0,0))
+
+        def _bfs() -> int:
+            length = 1
+            while queue:
+                for _ in range(len(queue)):
+                    r, c = queue.popleft()
+
+                    if r == ROWS - 1 and c == COLS - 1:
+                        return length
+
+                    for dr, dc in directions:
+                        nr = r + dr
+                        nc = c + dc
+
+
+                        if nr < 0 or nc < 0:
+                            continue
+                         
+                        if nr == ROWS or nc == COLS:
+                            continue
+
+                        if (nr, nc) in visited:
+                            continue
+
+                        if grid[nr][nc] == 1:
+                            continue
+
+                        visited.add((nr, nc))
+                        queue.append((nr, nc))
+                length += 1
+
+            return -1
+
+        return _bfs()
+        
