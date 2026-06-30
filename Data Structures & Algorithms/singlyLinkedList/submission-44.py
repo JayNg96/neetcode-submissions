b72145879a94f53d739bc512a6e9e4753ce3321c
NG JIAN WEI <74456400+JayNg96@users.noreply.github.com>
2026-07-01 04:55:16 +0800

    Add: singlyLinkedList - submission-44

---
+++ Data Structures & Algorithms/singlyLinkedList/submission-44.py
@@ -0,0 +1,58 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class LinkedList:
+    
+    def __init__(self):
+        self.head = ListNode(-1)
+        self.length = 0
+
+    def get(self, index: int) -> int:
+        if index >= self.length:
+            return -1
+
+        curr = self.head.next
+        for _ in range(index):
+            curr = curr.next
+
+        return curr.val
+
+    def insertHead(self, val: int) -> None:
+        curr = self.head
+        tmp = curr.next
+        new_head = ListNode(val)
+        new_head.next = tmp
+        curr.next = new_head
+        self.length += 1
+
+    def insertTail(self, val: int) -> None:
+        curr = self.head
+        while curr and curr.next:
+            curr = curr.next
+        
+        new_node = ListNode(val)
+        curr.next = new_node
+        self.length += 1
+
+    def remove(self, index: int) -> bool:
+        if index >= self.length:
+            return False
+        
+        curr = self.head
+        for _ in range(index):
+            curr = curr.next
+
+        curr.next = curr.next.next
+        self.length -= 1
+        return True
+
+    def getValues(self) -> List[int]:
+        curr = self.head.next
+        val_list = []
+        while curr:
+            val_list.append(curr.val)
+            curr = curr.next
+
+        return val_list
