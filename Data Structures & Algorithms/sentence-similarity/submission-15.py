513dfa39f97d355c6915bcfca073b5a6c314dca1
NG JIAN WEI <74456400+JayNg96@users.noreply.github.com>
2026-07-09 03:34:17 +0800

    Add: sentence-similarity - submission-15

---
+++ Data Structures & Algorithms/sentence-similarity/submission-15.py
@@ -0,0 +1,24 @@
+class Solution:
+    def areSentencesSimilar(self, sentence1: List[str], sentence2: List[str], similarPairs: List[List[str]]) -> bool:
+        if len(sentence1) != len(sentence2):
+            return False
+
+        def o1spaceSol():
+            for i, j in zip(sentence1, sentence2):
+                if [i,j] in similarPairs or [j,i] in similarPairs or i == j:
+                    continue
+                return False
+            return True
+
+        def o1timeSol():
+            similar_pair_dict = defaultdict(set)
+            for w1, w2 in similarPairs:
+                similar_pair_dict[w1].add(w2)
+                similar_pair_dict[w2].add(w1)
+
+            for w1, w2 in zip(sentence1, sentence2):
+                if w1 == w2 or w2 in similar_pair_dict[w1]:
+                    continue
+                return False
+            return True
+        return o1timeSol()
