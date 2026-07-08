607d1aa5bcaa54bcbbdfa16871058e24ce7dc546
NG JIAN WEI <74456400+JayNg96@users.noreply.github.com>
2026-07-09 07:48:03 +0800

    Add: first-unique-number - submission-1

---
+++ Data Structures & Algorithms/first-unique-number/submission-1.py
@@ -0,0 +1,21 @@
+class FirstUnique:
+
+    def __init__(self, nums: List[int]):
+        self.num_hashmap = defaultdict(int)
+        for n in nums:
+            self.num_hashmap[n] += 1
+
+    def showFirstUnique(self) -> int:
+        for num, count in self.num_hashmap.items():
+            if count == 1:
+                return num
+
+        return -1
+
+    def add(self, value: int) -> None:
+        self.num_hashmap[value] += 1
+
+# Your FirstUnique object will be instantiated and called as such:
+# obj = FirstUnique(nums)
+# param_1 = obj.showFirstUnique()
+# obj.add(value)
